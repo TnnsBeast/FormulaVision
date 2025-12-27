c4a4dcb353a96fc490145d5b739227cab6884f44
Neil Chulani <neil22@me.com>
2025-12-26 20:02:32 -0800

    Initial Commit

---
+++ formula/formulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  formulaApp.swift
+//  formula
+//
+//  Created by Neil Chulani on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct formulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ formulaTests/formulaTests.swift
@@ -0,0 +1,17 @@
+//
+//  formulaTests.swift
+//  formulaTests
+//
+//  Created by Neil Chulani on 12/26/25.
+//
+
+import Testing
+@testable import formula
+
+struct formulaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
